205768d456501012f894912aa9aeb8fdc76f6630
DavidPro80 <davidali1045@gmail.com>
2016-06-26 21:44:36 +0430

    Update rmsg.lua

--- plugins/rmsg.lua
+++ plugins/rmsg.lua
@@ -3,20 +3,20 @@ local function history(extra, suc, result)
     delete_msg(result[i].id, ok_cb, false)
   end
   if tonumber(extra.con) == #result then
-    send_msg(extra.chatid, '"'..#result..'" پیام اخیر سوپر گروه حذف شد', ok_cb, false)
+    send_msg(extra.chatid, '"'..#result..'" پیام اخیر سوپر گروه حذف شد ✅', ok_cb, false)
   else
-    send_msg(extra.chatid, 'تعداد پیام مورد نظر شما پاک شد', ok_cb, false)
+    send_msg(extra.chatid, 'تمام پیام های سوپر گروه حذف شد ✅', ok_cb, false)
   end
 end
 local function run(msg, matches)
   if matches[1] =='حذف' and is_owner(msg) then
     if msg.to.type == 'channel' then
-      if tonumber(matches[2]) > 100000000 or tonumber(matches[2]) < 1 then
-        return "تعداد بیشتر از 1 و کمتر از 100,000,000 مجازاست."
+      if tonumber(matches[2]) > 10000000 or tonumber(matches[2]) < 1 then
+        return "تعداد بیشتر از 1 و کمتر از 10,000,000 مجاز است."
       end
       get_history(msg.to.peer_id, matches[2] + 1 , history , {chatid = msg.to.peer_id, con = matches[2]})
     else
-      return "فقط در سوپرگروه ممکن است."
+      return "قثط در سوپرگروه امکان حذف پیام است."
     end
   else
     return "شما دسترسی ندارید."
